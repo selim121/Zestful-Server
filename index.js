7cbcac4a3b461ae98733ba97c3c4c79368be146b
selim121 <selimkhan.bdkhan@gmail.com>
2023-05-03 16:32:55 +0600

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
 
 const allData = require('./data/allData.json');
 
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('Zestful is running')
 });
