26df288dfc0a4e996a70a21db8afc3966f240743
selim121 <selimkhan.bdkhan@gmail.com>
2023-05-03 11:59:53 +0600

    data load on localhost

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+const allData = require('./data/allData.json');
+
 app.get('/', (req, res) => {
     res.send('Zestful is running')
 });
+
+app.get('/allData', (req, res) => {
+    res.send(allData);
+})
 
 app.listen(port, () => {
     console.log(`Zestful API is running on port: ${port}`);
